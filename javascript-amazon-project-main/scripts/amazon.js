3da8ef451140075ed89fa9461510cabb00689e36
zak Dev <zakariabeggar237@gmail.com>
2025-02-09 19:36:48 +0100

    start writing js

---
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -0,0 +1,9 @@
+const products=[{
+    image:'images/products/athletic-cotton-socks-6-pairs.jpg',
+    name:' Black and Gray Athletic Cotton Socks - 6 Pairs',
+    rating:{
+        stars:4.5,
+        count:87
+    },
+    priceCents: 1090
+},];
