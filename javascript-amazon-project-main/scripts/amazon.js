b36a037702e6644deb9ec8a1ad9d4a48f5676566
zak Dev <zakariabeggar237@gmail.com>
2025-02-10 10:52:50 +0100

    use products.js file

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,29 +1,4 @@
-const products=[{
-    image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name:' Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating:{
-        stars:4.5,
-        count:87
-    },
-    priceCents: 1090
-},{
-    image:'images/products/intermediate-composite-basketball.jpg' ,
-    name:'Intermediate Size Basketball' ,
-    rating:{
-        stars:4 ,
-        count: 127
-    },
-    priceCents:2095 
-},{
-    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name:' Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-        stars:4.5 ,
-        count:56
-        },
-        priceCents:799
-},
-];
+
 let productHTML='' ;
 products.forEach((product)=>{
    // const image = product.image ;
@@ -47,7 +22,7 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-price">
-            $${product.priceCents /100} 
+            $${(product.priceCents /100).toFixed(2)} 
           </div>
 
           <div class="product-quantity-container">
@@ -76,6 +51,8 @@ products.forEach((product)=>{
           </button>
         </div>
     `;
+    document.querySelector('.js-products-grid')
+    .innerHTML= productHTML ;
     
-console.log(html) ;
+
 }) ;
