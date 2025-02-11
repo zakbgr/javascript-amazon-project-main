c8df47096b8ffe4d3d024d9ab74d1ad93da9cc6f
zak Dev <zakariabeggar237@gmail.com>
2025-02-11 18:52:33 +0100

    adding the cart quantity calculation

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,5 +1,6 @@
 import { products } from '../data/products.js';
 import {cart} from '../data/cart.js' ;
+
 let productHTML='' ;
 products.forEach((product)=>{
    // const image = product.image ;
@@ -27,28 +28,28 @@ products.forEach((product)=>{
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
+              <option value="3" >3</option>
+              <option value="4" >4</option>
+              <option value="5" >5</option>
+              <option value="6" >6</option>
+              <option value="7" >7</option>
+              <option value="8"  >8</option>
+              <option value="9"  >9</option>
+              <option value="10" >10</option>
             </select>
           </div>
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-check-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
           <button class="js-add-to-cart add-to-cart-button button-primary"
-          data-product-name="${product.name}">
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -58,24 +59,40 @@ products.forEach((product)=>{
     
 
 }) ;
-
+let stopTime  ;
+const addedMessageTimeouts = {};
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) =>{ 
   button.addEventListener('click', () => {
   //console.log('added to cart') ;
-  const productName = button.dataset.productName ;
+  const productId = button.dataset.productId ;
   let matchingItem='' ;
 cart.forEach((item)=>{
-  if(productName === item.productName){
+  if(productId === item.productId){
     matchingItem= item ; 
   }
 });
+/*
+// Display added to cart message
+const addedToCartMessage = document.querySelector(`.js-check-${productId}`);
+addedToCartMessage.classList.add('js-added-msg') ;
+// Remove button functionality
+clearTimeout(stopTime) ;
+stopTime = setTimeout(()=>{
+  document.querySelector('.js-added-msg').remove() ;
+} ,2000) ;
+*/
+
+
+const quantitySelector =document.querySelector(`.js-quantity-selector-${productId}`) ;
+//console.log(quantitySelector.value)
+const quantity = parseInt(quantitySelector.value) ;
   if(matchingItem){
-    matchingItem.quantity += 1 ;
+    matchingItem.quantity += quantity ;
   }else{ 
   cart.push({
-      productName:productName,
-      quantity: 1
+      productId,
+     quantity
   });
 }
 let cartQuantity= 0 ;
@@ -85,9 +102,34 @@ cart.forEach((item)=>{
 
 let cardQ= document.querySelector('.js-quantity');
 cardQ.textContent = cartQuantity ;
+ // save cart into a local storage 
+ //localStorage.setItem('cart',JSON.stringify(cart)) ;
+
+const addedToCartMessage =document.querySelector(`.js-check-${productId}`);
+
+addedToCartMessage.classList.add('js-added-msg');
+setTimeout(() => {
+  
+  const oldTimeout = addedMessageTimeouts[productId] ;
+  if(oldTimeout){
+    clearTimeout(oldTimeout) ;
+  }
+  const timeOutId = setTimeout(() => {
+    addedToCartMessage.classList.remove('js-added-msg') ;
+  }, 2000);
 
 
-})});
+  addedMessageTimeouts[productId] = timeOutId ;
+}, 2000);
+})
+
+}
+
+);
+
+
+
 
 //save quantity into a local storage 
+
 
